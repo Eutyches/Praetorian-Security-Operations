207a628f8d7efd62d89c7626aa0286e6704e7b59
big-corey <39989971+big-corey@users.noreply.github.com>
2019-08-29 18:35:06 -0600

    Added ACEX w/Sitting 3.3.1

---
+++ @acex_modded/mod.cpp
@@ -0,0 +1,12 @@
+name = "Advanced Combat Environment Extras 3.4.2";
+picture = "logo_acex_ca.paa";
+actionName = "GitHub";
+action = "https://github.com/acemod/ACEX";
+description = "ACEX - Version 3.4.2";
+logo = "logo_acex_ca.paa";
+logoOver = "logo_acex_ca.paa";
+tooltip = "ACEX";
+tooltipOwned = "ACEX Owned";
+overview = "Advanced Combat Environment Extras, also known as ACEX, adds a variety of additional systems on top of the ACE3 framework.";
+author = "ACE3 Team";
+overviewPicture = "logo_acex_ca.paa";
