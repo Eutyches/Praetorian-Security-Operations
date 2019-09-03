5699e6ab383fd61e73b49055e56c46d55be3a50b
big-corey <39989971+big-corey@users.noreply.github.com>
2019-09-03 00:58:16 -0600

    Pistol Reskin 0.2

--- @pso/addons/pso_weapons/config.cpp
+++ @pso/addons/pso_weapons/config.cpp
@@ -14,19 +14,19 @@ class CfgWeapons {
 	//class hgun_ACP2_F;
 	class hgun_P07_F;
 	
-/*	class pso_p07_black : hgun_P07_F {
-		displayname = "P07 9mm PSO Black";
+	class pso_p07_m81 : hgun_P07_F {
+		displayname = "P07 9mm PSO M81";
 		hiddenSelections[] = {"Camo"};
-		hiddenSelectionsTextures[] = {"\pso_weapons\data\p07_black.paa"};
-		baseweapon = pso_p07_black;
+		hiddenSelectionsTextures[] = {"\pso_weapons\data\p07_m81.paa"};
+		baseweapon = pso_p07_m81;
 	};
 	
-	class pso_p07_olive : hgun_P07_F {
-		displayname = "P07 9mm PSO Olive";
+	class pso_p07_multicam : hgun_P07_F {
+		displayname = "P07 9mm PSO Multicam";
 		hiddenSelections[] = {"Camo"};
-		hiddenSelectionsTextures[] = {"\pso_weapons\data\p07_olive.paa"};
-		baseweapon = pso_p07_olive;
-	}; */
+		hiddenSelectionsTextures[] = {"\pso_weapons\data\p07_multicam.paa"};
+		baseweapon = pso_p07_multicam;
+	};
 	
 	class pso_p07_grey : hgun_P07_F {
 		displayname = "P07 9mm PSO Grey";
@@ -34,4 +34,11 @@ class CfgWeapons {
 		hiddenSelectionsTextures[] = {"\pso_weapons\data\p07_grey.paa"};
 		baseweapon = pso_p07_grey;
 	};
+	
+	class pso_p07_scratched : hgun_P07_F {
+		displayname = "P07 9mm PSO Scratched";
+		hiddenSelections[] = {"Camo"};
+		hiddenSelectionsTextures[] = {"\pso_weapons\data\p07_scratched.paa"};
+		baseweapon = pso_p07_scratched;
+	};	
 };
