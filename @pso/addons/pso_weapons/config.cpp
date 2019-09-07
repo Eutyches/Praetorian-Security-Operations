7b7924963c5741bd9789f94f4c022d646b83a07a
big-corey <39989971+big-corey@users.noreply.github.com>
2019-09-06 20:50:07 -0600

    Testing

--- @pso/addons/pso_weapons/config.cpp
+++ @pso/addons/pso_weapons/config.cpp
@@ -40,5 +40,52 @@ class CfgWeapons {
 		hiddenSelections[] = {"Camo"};
 		hiddenSelectionsTextures[] = {"\pso_weapons\data\p07_scratched.paa"};
 		baseweapon = pso_p07_scratched;
+	};
+
+	class rhs_weap_m249_pip_S;
+	class RHS_M249_Snack: rhs_weap_m249_pip_S
+	{
+		author = "FlankenLabs/ModifiedbyCorey";
+		displayName = "M249 Snack Edition";
+		picture = "\rhsusf\addons\rhsusf_inventoryicons\data\weapons\rhs_weap_m249_ca.paa";
+		hiddenSelectionsTextures[]=
+		{
+			"pso_weapons\data\m249_camo\reciever_249_co.paa",
+			"pso_weapons\data\m249_camo\m249_merged1_co.paa",
+			"pso_weapons\data\m249_camo\barrelstock_merged_co.paa",
+			"pso_weapons\data\m249_camo\ammobox_merged_co.paa",
+			"pso_weapons\data\m249_camo\Stock_co.paa"
+		};
+		baseWeapon="RHS_M249_Snack"
+		rhs_grip1_change="RHS_M249_Snack_vfg1";
+		rhs_grip2_change="RHS_M249_Snack_vfg2";
+		rhs_grip3_change="RHS_M249_Snack_vfg3";
+	};
+	class RHS_M249_Snack_vfg1: RHS_M249_Snack
+	{
+		author="FlankenLabs/ModifiedbyCorey";
+		handanim[]=
+		{
+			"OFP2_ManSkeleton",
+			"\rhsusf\addons\rhsusf_c_weapons\anims\rhs_hand_m249_grippod.rtm"
+		};
+	};
+	class RHS_M249_Snack_vfg2: RHS_M249_Snack
+	{
+		author="FlankenLabs/ModifiedbyCorey";
+		handanim[]=
+		{
+			"OFP2_ManSkeleton",
+			"\rhsusf\addons\rhsusf_c_weapons\anims\rhs_hand_m249_VFG.rtm"
+		};
+	};
+	class RHS_M249_Snack_vfg3: RHS_M249_Snack
+	{
+		author="FlankenLabs";
+		handanim[]=
+		{
+			"OFP2_ManSkeleton",
+			"\rhsusf\addons\rhsusf_c_weapons\anims\rhs_hand_m249_grippod.rtm"
+		};
 	};	
 };
