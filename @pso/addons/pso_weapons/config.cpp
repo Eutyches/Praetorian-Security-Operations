b1a228585b357908a6d392ab765799c494ae7875
big-corey <39989971+big-corey@users.noreply.github.com>
2019-09-16 02:37:09 -0600

    COS file path fix

--- @pso/addons/pso_weapons/config.cpp
+++ @pso/addons/pso_weapons/config.cpp
@@ -1,4 +1,4 @@
-//Config adapted from LM Handguns by Corey. All texturing done from scratch by Corey.
+//Config adapted from the configs of LM Handguns & FlankenLabs. All texturing done from scratch by Corey.
 
 class CfgPatches {
 	class pso_weapons {
@@ -17,36 +17,36 @@ class CfgWeapons {
 	class pso_p07_m81 : hgun_P07_F {
 		displayname = "P07 9mm PSO M81";
 		hiddenSelections[] = {"Camo"};
-		hiddenSelectionsTextures[] = {"\pso_weapons\data\p07_m81.paa"};
+		hiddenSelectionsTextures[] = {"\pso_weapons\data\p07\p07_m81.paa"};
 		baseweapon = pso_p07_m81;
 	};
 	
 	class pso_p07_multicam : hgun_P07_F {
 		displayname = "P07 9mm PSO Multicam";
 		hiddenSelections[] = {"Camo"};
-		hiddenSelectionsTextures[] = {"\pso_weapons\data\p07_multicam.paa"};
+		hiddenSelectionsTextures[] = {"\pso_weapons\data\p07\p07_multicam.paa"};
 		baseweapon = pso_p07_multicam;
 	};
 	
 	class pso_p07_grey : hgun_P07_F {
 		displayname = "P07 9mm PSO Grey";
 		hiddenSelections[] = {"Camo"};
-		hiddenSelectionsTextures[] = {"\pso_weapons\data\p07_grey.paa"};
+		hiddenSelectionsTextures[] = {"\pso_weapons\data\p07\p07_grey.paa"};
 		baseweapon = pso_p07_grey;
 	};
 	
 	class pso_p07_scratched : hgun_P07_F {
 		displayname = "P07 9mm PSO Scratched";
 		hiddenSelections[] = {"Camo"};
-		hiddenSelectionsTextures[] = {"\pso_weapons\data\p07_scratched.paa"};
+		hiddenSelectionsTextures[] = {"\pso_weapons\data\p07\p07_scratched.paa"};
 		baseweapon = pso_p07_scratched;
 	};
 
 	class rhs_weap_m249_pip_S;
-	class RHS_M249_Snack: rhs_weap_m249_pip_S
+	class RHS_M249_Snackbar: rhs_weap_m249_pip_S
 	{
-		author = "FlankenLabs/ModifiedbyCorey";
-		displayName = "M249 Snack Edition";
+		author = "Corey";
+		displayName = "M249 Snackbar Edition";
 		picture = "\rhsusf\addons\rhsusf_inventoryicons\data\weapons\rhs_weap_m249_ca.paa";
 		hiddenSelectionsTextures[]=
 		{
@@ -56,36 +56,81 @@ class CfgWeapons {
 			"pso_weapons\data\m249_camo\ammobox_merged_co.paa",
 			"pso_weapons\data\m249_camo\Stock_co.paa"
 		};
-		baseWeapon="RHS_M249_Snack"
-		rhs_grip1_change="RHS_M249_Snack_vfg1";
-		rhs_grip2_change="RHS_M249_Snack_vfg2";
-		rhs_grip3_change="RHS_M249_Snack_vfg3";
+		baseWeapon="RHS_M249_Snackbar"
+		rhs_grip1_change="RHS_M249_Snackbar_vfg1";
+		rhs_grip2_change="RHS_M249_Snackbar_vfg2";
+		rhs_grip3_change="RHS_M249_Snackbar_vfg3";
 	};
-	class RHS_M249_Snack_vfg1: RHS_M249_Snack
+	class RHS_M249_Snackbar_vfg1: RHS_M249_Snackbar
 	{
-		author="FlankenLabs/ModifiedbyCorey";
+		author="Corey";
 		handanim[]=
 		{
 			"OFP2_ManSkeleton",
 			"\rhsusf\addons\rhsusf_c_weapons\anims\rhs_hand_m249_grippod.rtm"
 		};
 	};
-	class RHS_M249_Snack_vfg2: RHS_M249_Snack
+	class RHS_M249_Snackbar_vfg2: RHS_M249_Snackbar
 	{
-		author="FlankenLabs/ModifiedbyCorey";
+		author="Corey";
 		handanim[]=
 		{
 			"OFP2_ManSkeleton",
 			"\rhsusf\addons\rhsusf_c_weapons\anims\rhs_hand_m249_VFG.rtm"
 		};
 	};
-	class RHS_M249_Snack_vfg3: RHS_M249_Snack
+	class RHS_M249_Snackbar_vfg3: RHS_M249_Snackbar
 	{
-		author="FlankenLabs";
+		author="Corey";
 		handanim[]=
 		{
 			"OFP2_ManSkeleton",
 			"\rhsusf\addons\rhsusf_c_weapons\anims\rhs_hand_m249_grippod.rtm"
 		};
-	};	
+	};
+class RHS_M249_Snackbar_Weathered: rhs_weap_m249_pip_S
+	{
+		author = "Corey";
+		displayName = "M249 Snackbar Weathered Edition";
+		picture = "\rhsusf\addons\rhsusf_inventoryicons\data\weapons\rhs_weap_m249_ca.paa";
+		hiddenSelectionsTextures[]=
+		{
+			"pso_weapons\data\m249_camo\reciever_249_weathered_co.paa",
+			"pso_weapons\data\m249_camo\m249_merged1_weathered_co.paa",
+			"pso_weapons\data\m249_camo\barrelstock_merged_weathered_co.paa",
+			"pso_weapons\data\m249_camo\ammobox_merged_weathered_co.paa",
+			"pso_weapons\data\m249_camo\Stock_weathered_co.paa"
+		};
+		baseWeapon="RHS_M249_Snackbar_Weathered"
+		rhs_grip1_change="RHS_M249_Snackbar_Weathered_vfg1";
+		rhs_grip2_change="RHS_M249_Snackbar_Weathered_vfg2";
+		rhs_grip3_change="RHS_M249_Snackbar_Weathered_vfg3";
+	};
+	class RHS_M249_Snackbar_Weathered_vfg1: RHS_M249_Snackbar_Weathered
+	{
+		author="Corey";
+		handanim[]=
+		{
+			"OFP2_ManSkeleton",
+			"\rhsusf\addons\rhsusf_c_weapons\anims\rhs_hand_m249_grippod.rtm"
+		};
+	};
+	class RHS_M249_Snackbar_Weathered_vfg2: RHS_M249_Snackbar_Weathered
+	{
+		author="Corey";
+		handanim[]=
+		{
+			"OFP2_ManSkeleton",
+			"\rhsusf\addons\rhsusf_c_weapons\anims\rhs_hand_m249_VFG.rtm"
+		};
+	};
+	class RHS_M249_Snackbar_Weathered_vfg3: RHS_M249_Snackbar_Weathered
+	{
+		author="Corey";
+		handanim[]=
+		{
+			"OFP2_ManSkeleton",
+			"\rhsusf\addons\rhsusf_c_weapons\anims\rhs_hand_m249_grippod.rtm"
+		};
+	};		
 };
