9efd9d14b738bcfce8b52520130c88cd18ab31e8
big-corey <39989971+big-corey@users.noreply.github.com>
2019-09-20 19:16:52 -0600

    Flag Hat

--- @pso/addons/snackpack_headgear/config.cpp
+++ @pso/addons/snackpack_headgear/config.cpp
@@ -9196,6 +9196,276 @@ class snackpack_h_cap_01_red_uk: ItemCore
 			};
 		};
 	};
+class snackpack_h_cap_01_red_wales: ItemCore
+	{
+		author="Adacas (retexture by Snackbar)";
+		scope=2;
+		weaponPoolAvailable=1;
+		displayName="Wales (Red)";
+		picture="\snackpack_headgear\ui\icon_h_cap_CB_ca.paa";
+		model="\snackpack_headgear\models\h_cap_base.p3d";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo1",
+			"_ESS",
+			"_Surefire",
+			"_Peltor"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\snackpack_headgear\textures\h_cap_red_co_wales.paa",
+			"\snackpack_headgear\textures\h_gear_01_CB_co.paa"
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass=10;
+			uniformModel="\snackpack_headgear\models\h_cap_base.p3d";
+			modelSides[]=
+			{
+				"TWest"
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo1",
+				"_ESS",
+				"_Surefire",
+				"_Peltor"
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName="HitHead";
+					armor=6;
+					passThrough=.5;
+				};
+			};
+		};
+	};
+	class snackpack_h_cap_01_goggles_red_wales: ItemCore
+	{
+		author="Adacas (retexture by Snackbar)";
+		scope=2;
+		weaponPoolAvailable=1;
+		displayName="Wales + Goggles (Red)";
+		picture="\snackpack_headgear\ui\icon_h_cap_CB_ca.paa";
+		model="\snackpack_headgear\models\h_cap_base.p3d";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo1",
+			"_Surefire",
+			"_Peltor"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\snackpack_headgear\textures\h_cap_red_co_wales.paa",
+			"\snackpack_headgear\textures\h_gear_01_CB_co.paa"
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass=10;
+			uniformModel="\snackpack_headgear\models\h_cap_base.p3d";
+			modelSides[]=
+			{
+				"TWest"
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo1",
+				"_Surefire",
+				"_Peltor"
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName="HitHead";
+					armor=6;
+					passThrough=.5;
+				};
+			};
+		};
+	};
+	class snackpack_h_cap_02_red_wales: ItemCore
+	{
+		author="Adacas (retexture by Snackbar)";
+		scope=2;
+		weaponPoolAvailable=1;
+		displayName="Wales (Red)";
+		picture="\snackpack_headgear\ui\icon_h_cap_CB_ca.paa";
+		model="\snackpack_headgear\models\h_cap_base.p3d";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo1",
+			"_ESS",
+			"_Surefire"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\snackpack_headgear\textures\h_cap_red_co_wales.paa",
+			"\snackpack_headgear\textures\h_gear_01_CB_co.paa"
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass=10;
+			uniformModel="\snackpack_headgear\models\h_cap_base.p3d";
+			modelSides[]=
+			{
+				"TWest"
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo1",
+				"_ESS",
+				"_Surefire"
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName="HitHead";
+					armor=6;
+					passThrough=.5;
+				};
+			};
+		};
+	};
+	class snackpack_h_cap_02_goggles_red_wales: ItemCore
+	{
+		author="Adacas (retexture by Snackbar)";
+		scope=2;
+		weaponPoolAvailable=1;
+		displayName="Wales + Goggles (Red)";
+		picture="\snackpack_headgear\ui\icon_h_cap_CB_ca.paa";
+		model="\snackpack_headgear\models\h_cap_base.p3d";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo1",
+			"_Surefire"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\snackpack_headgear\textures\h_cap_red_co_wales.paa",
+			"\snackpack_headgear\textures\h_gear_01_CB_co.paa"
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass=10;
+			uniformModel="\snackpack_headgear\models\h_cap_base.p3d";
+			modelSides[]=
+			{
+				"TWest"
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo1",
+				"_Surefire"
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName="HitHead";
+					armor=6;
+					passThrough=.5;
+				};
+			};
+		};
+	};
+	class snackpack_h_cap_03_red_wales: ItemCore
+	{
+		author="Adacas (retexture by Snackbar)";
+		scope=2;
+		weaponPoolAvailable=1;
+		displayName="Wales (Red)";
+		picture="\snackpack_headgear\ui\icon_h_cap_CB_ca.paa";
+		model="\snackpack_headgear\models\h_cap_base.p3d";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo1",
+			"_ESS"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\snackpack_headgear\textures\h_cap_red_co_wales.paa",
+			"\snackpack_headgear\textures\h_gear_01_CB_co.paa"
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass=10;
+			uniformModel="\snackpack_headgear\models\h_cap_base.p3d";
+			modelSides[]=
+			{
+				"TWest"
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo1",
+				"_ESS"
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName="HitHead";
+					armor=6;
+					passThrough=.5;
+				};
+			};
+		};
+	};
+	class snackpack_h_cap_03_goggles_red_wales: ItemCore
+	{
+		author="Adacas (retexture by Snackbar)";
+		scope=2;
+		weaponPoolAvailable=1;
+		displayName="Wales + Goggles (Red)";
+		picture="\snackpack_headgear\ui\icon_h_cap_CB_ca.paa";
+		model="\snackpack_headgear\models\h_cap_base.p3d";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo1"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\snackpack_headgear\textures\h_cap_red_co_wales.paa",
+			"\snackpack_headgear\textures\h_gear_01_CB_co.paa"
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass=10;
+			uniformModel="\snackpack_headgear\models\h_cap_base.p3d";
+			modelSides[]=
+			{
+				"TWest"
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo1"
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName="HitHead";
+					armor=6;
+					passThrough=.5;
+				};
+			};
+		};
+	};
 class snackpack_h_cap_01_red_gam: ItemCore
 	{
 		author="Adacas (retexture by Snackbar)";
