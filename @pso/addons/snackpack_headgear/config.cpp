b37003ce2dc60a8b1e0cd3854853d4561db15d3e
big-corey <39989971+big-corey@users.noreply.github.com>
2019-09-28 18:29:14 -0600

    Heagear 2.0 pt. 1

--- @pso/addons/snackpack_headgear/config.cpp
+++ @pso/addons/snackpack_headgear/config.cpp
@@ -13516,6 +13516,546 @@ class snackpack_h_cap_01_od_pso_white: ItemCore
 			};
 		};
 	};
+class snackpack_h_cap_01_white_pso_black: ItemCore
+	{
+		author="Adacas (retexture by Corey)";
+		scope=2;
+		weaponPoolAvailable=1;
+		displayName="PSO White (Black Logo)";
+		picture="\snackpack_headgear\ui\icon_h_cap_CB_ca.paa";
+		model="\snackpack_headgear\models\h_cap_base.p3d";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo1",
+			"_ESS",
+			"_Surefire",
+			"_Peltor"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\snackpack_headgear\textures\h_cap_white_co_pso_black.paa",
+			"\snackpack_headgear\textures\h_gear_01_CB_co.paa"
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass=10;
+			uniformModel="\snackpack_headgear\models\h_cap_base.p3d";
+			modelSides[]=
+			{
+				"TWest"
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo1",
+				"_ESS",
+				"_Surefire",
+				"_Peltor"
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName="HitHead";
+					armor=6;
+					passThrough=.5;
+				};
+			};
+		};
+	};
+	class snackpack_h_cap_01_goggles_white_pso_black: ItemCore
+	{
+		author="Adacas (retexture by Corey)";
+		scope=2;
+		weaponPoolAvailable=1;
+		displayName="PSO White + Goggles (Black Logo)";
+		picture="\snackpack_headgear\ui\icon_h_cap_CB_ca.paa";
+		model="\snackpack_headgear\models\h_cap_base.p3d";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo1",
+			"_Surefire",
+			"_Peltor"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\snackpack_headgear\textures\h_cap_white_co_pso_black.paa",
+			"\snackpack_headgear\textures\h_gear_01_CB_co.paa"
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass=10;
+			uniformModel="\snackpack_headgear\models\h_cap_base.p3d";
+			modelSides[]=
+			{
+				"TWest"
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo1",
+				"_Surefire",
+				"_Peltor"
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName="HitHead";
+					armor=6;
+					passThrough=.5;
+				};
+			};
+		};
+	};
+	class snackpack_h_cap_02_white_pso_black: ItemCore
+	{
+		author="Adacas (retexture by Corey)";
+		scope=2;
+		weaponPoolAvailable=1;
+		displayName="PSO White (Black Logo)";
+		picture="\snackpack_headgear\ui\icon_h_cap_CB_ca.paa";
+		model="\snackpack_headgear\models\h_cap_base.p3d";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo1",
+			"_ESS",
+			"_Surefire"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\snackpack_headgear\textures\h_cap_white_co_pso_black.paa",
+			"\snackpack_headgear\textures\h_gear_01_CB_co.paa"
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass=10;
+			uniformModel="\snackpack_headgear\models\h_cap_base.p3d";
+			modelSides[]=
+			{
+				"TWest"
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo1",
+				"_ESS",
+				"_Surefire"
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName="HitHead";
+					armor=6;
+					passThrough=.5;
+				};
+			};
+		};
+	};
+	class snackpack_h_cap_02_goggles_white_pso_black: ItemCore
+	{
+		author="Adacas (retexture by Corey)";
+		scope=2;
+		weaponPoolAvailable=1;
+		displayName="PSO White + Goggles (Black Logo)";
+		picture="\snackpack_headgear\ui\icon_h_cap_CB_ca.paa";
+		model="\snackpack_headgear\models\h_cap_base.p3d";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo1",
+			"_Surefire"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\snackpack_headgear\textures\h_cap_white_co_pso_black.paa",
+			"\snackpack_headgear\textures\h_gear_01_CB_co.paa"
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass=10;
+			uniformModel="\snackpack_headgear\models\h_cap_base.p3d";
+			modelSides[]=
+			{
+				"TWest"
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo1",
+				"_Surefire"
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName="HitHead";
+					armor=6;
+					passThrough=.5;
+				};
+			};
+		};
+	};
+	class snackpack_h_cap_03_white_pso_black: ItemCore
+	{
+		author="Adacas (retexture by Corey)";
+		scope=2;
+		weaponPoolAvailable=1;
+		displayName="PSO White (Black Logo)";
+		picture="\snackpack_headgear\ui\icon_h_cap_CB_ca.paa";
+		model="\snackpack_headgear\models\h_cap_base.p3d";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo1",
+			"_ESS"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\snackpack_headgear\textures\h_cap_white_co_pso_black.paa",
+			"\snackpack_headgear\textures\h_gear_01_CB_co.paa"
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass=10;
+			uniformModel="\snackpack_headgear\models\h_cap_base.p3d";
+			modelSides[]=
+			{
+				"TWest"
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo1",
+				"_ESS"
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName="HitHead";
+					armor=6;
+					passThrough=.5;
+				};
+			};
+		};
+	};
+	class snackpack_h_cap_03_goggles_white_pso_black: ItemCore
+	{
+		author="Adacas (retexture by Corey)";
+		scope=2;
+		weaponPoolAvailable=1;
+		displayName="PSO White + Goggles (Black Logo)";
+		picture="\snackpack_headgear\ui\icon_h_cap_CB_ca.paa";
+		model="\snackpack_headgear\models\h_cap_base.p3d";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo1"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\snackpack_headgear\textures\h_cap_white_co_pso_black.paa",
+			"\snackpack_headgear\textures\h_gear_01_CB_co.paa"
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass=10;
+			uniformModel="\snackpack_headgear\models\h_cap_base.p3d";
+			modelSides[]=
+			{
+				"TWest"
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo1"
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName="HitHead";
+					armor=6;
+					passThrough=.5;
+				};
+			};
+		};
+	};
+class snackpack_h_cap_01_white_pso_color: ItemCore
+	{
+		author="Adacas (retexture by Corey)";
+		scope=2;
+		weaponPoolAvailable=1;
+		displayName="PSO White (Color Logo)";
+		picture="\snackpack_headgear\ui\icon_h_cap_CB_ca.paa";
+		model="\snackpack_headgear\models\h_cap_base.p3d";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo1",
+			"_ESS",
+			"_Surefire",
+			"_Peltor"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\snackpack_headgear\textures\h_cap_white_co_pso_color.paa",
+			"\snackpack_headgear\textures\h_gear_01_CB_co.paa"
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass=10;
+			uniformModel="\snackpack_headgear\models\h_cap_base.p3d";
+			modelSides[]=
+			{
+				"TWest"
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo1",
+				"_ESS",
+				"_Surefire",
+				"_Peltor"
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName="HitHead";
+					armor=6;
+					passThrough=.5;
+				};
+			};
+		};
+	};
+	class snackpack_h_cap_01_goggles_white_pso_color: ItemCore
+	{
+		author="Adacas (retexture by Corey)";
+		scope=2;
+		weaponPoolAvailable=1;
+		displayName="PSO White + Goggles (Color Logo)";
+		picture="\snackpack_headgear\ui\icon_h_cap_CB_ca.paa";
+		model="\snackpack_headgear\models\h_cap_base.p3d";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo1",
+			"_Surefire",
+			"_Peltor"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\snackpack_headgear\textures\h_cap_white_co_pso_color.paa",
+			"\snackpack_headgear\textures\h_gear_01_CB_co.paa"
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass=10;
+			uniformModel="\snackpack_headgear\models\h_cap_base.p3d";
+			modelSides[]=
+			{
+				"TWest"
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo1",
+				"_Surefire",
+				"_Peltor"
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName="HitHead";
+					armor=6;
+					passThrough=.5;
+				};
+			};
+		};
+	};
+	class snackpack_h_cap_02_white_pso_color: ItemCore
+	{
+		author="Adacas (retexture by Corey)";
+		scope=2;
+		weaponPoolAvailable=1;
+		displayName="PSO White (Color Logo)";
+		picture="\snackpack_headgear\ui\icon_h_cap_CB_ca.paa";
+		model="\snackpack_headgear\models\h_cap_base.p3d";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo1",
+			"_ESS",
+			"_Surefire"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\snackpack_headgear\textures\h_cap_white_co_pso_color.paa",
+			"\snackpack_headgear\textures\h_gear_01_CB_co.paa"
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass=10;
+			uniformModel="\snackpack_headgear\models\h_cap_base.p3d";
+			modelSides[]=
+			{
+				"TWest"
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo1",
+				"_ESS",
+				"_Surefire"
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName="HitHead";
+					armor=6;
+					passThrough=.5;
+				};
+			};
+		};
+	};
+	class snackpack_h_cap_02_goggles_white_pso_color: ItemCore
+	{
+		author="Adacas (retexture by Corey)";
+		scope=2;
+		weaponPoolAvailable=1;
+		displayName="PSO White + Goggles (Color Logo)";
+		picture="\snackpack_headgear\ui\icon_h_cap_CB_ca.paa";
+		model="\snackpack_headgear\models\h_cap_base.p3d";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo1",
+			"_Surefire"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\snackpack_headgear\textures\h_cap_white_co_pso_color.paa",
+			"\snackpack_headgear\textures\h_gear_01_CB_co.paa"
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass=10;
+			uniformModel="\snackpack_headgear\models\h_cap_base.p3d";
+			modelSides[]=
+			{
+				"TWest"
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo1",
+				"_Surefire"
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName="HitHead";
+					armor=6;
+					passThrough=.5;
+				};
+			};
+		};
+	};
+	class snackpack_h_cap_03_white_pso_color: ItemCore
+	{
+		author="Adacas (retexture by Corey)";
+		scope=2;
+		weaponPoolAvailable=1;
+		displayName="PSO White (Color Logo)";
+		picture="\snackpack_headgear\ui\icon_h_cap_CB_ca.paa";
+		model="\snackpack_headgear\models\h_cap_base.p3d";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo1",
+			"_ESS"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\snackpack_headgear\textures\h_cap_white_co_pso_color.paa",
+			"\snackpack_headgear\textures\h_gear_01_CB_co.paa"
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass=10;
+			uniformModel="\snackpack_headgear\models\h_cap_base.p3d";
+			modelSides[]=
+			{
+				"TWest"
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo1",
+				"_ESS"
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName="HitHead";
+					armor=6;
+					passThrough=.5;
+				};
+			};
+		};
+	};
+	class snackpack_h_cap_03_goggles_white_pso_color: ItemCore
+	{
+		author="Adacas (retexture by Corey)";
+		scope=2;
+		weaponPoolAvailable=1;
+		displayName="PSO White + Goggles (Color Logo)";
+		picture="\snackpack_headgear\ui\icon_h_cap_CB_ca.paa";
+		model="\snackpack_headgear\models\h_cap_base.p3d";
+		hiddenSelections[]=
+		{
+			"camo",
+			"camo1"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"\snackpack_headgear\textures\h_cap_white_co_pso_color.paa",
+			"\snackpack_headgear\textures\h_gear_01_CB_co.paa"
+		};
+		class ItemInfo: HeadgearItem
+		{
+			mass=10;
+			uniformModel="\snackpack_headgear\models\h_cap_base.p3d";
+			modelSides[]=
+			{
+				"TWest"
+			};
+			hiddenSelections[]=
+			{
+				"camo",
+				"camo1"
+			};
+			class HitpointsProtectionInfo
+			{
+				class Head
+				{
+					hitPointName="HitHead";
+					armor=6;
+					passThrough=.5;
+				};
+			};
+		};
+	};
 //pants
 	class snackpack_h_cap_01_rgr_pants: ItemCore
 	{
