61ded4db9d3266570b81b5ce69bd06070ffa99c4
big-corey <39989971+big-corey@users.noreply.github.com>
2019-11-01 18:48:21 -0600

    Foxhound

--- @pso/addons/pso_foxhound/config.cpp
+++ @pso/addons/pso_foxhound/config.cpp
@@ -1,20 +1,20 @@
 class CfgPatches
 {
-	class PSO_Foxhound
+	class pso_foxhound
 	{
-		units[] = {"rksla3_foxhound_lppv_tes_pso_black","rksla3_foxhound_lppv_tes_pso_olive","rksla3_foxhound_lppv_tes_pso_tan","rksla3_foxhound_lppv_tes_pso_white"};
+		units[] = {"rksla3_foxhound_lppv_base_pso_black","rksla3_foxhound_lppv_base_pso_olive","rksla3_foxhound_lppv_base_pso_tan","rksla3_foxhound_lppv_base_pso_white"};
 		weapons[] = {};
 		requiredVersion = 0.1;
-		requiredAddons[] = {};
+		requiredAddons[] = {"rksla3_foxhound_lppv"};
 		authors[] = {"Corey"};
 	};
 };
 
 class CfgVehicles
 {
-	class rksla3_foxhound_lppv_tes;
+	class rksla3_foxhound_lppv_base;
 	
-	class rksla3_foxhound_lppv_tes_pso_black: rksla3_foxhound_lppv_tes
+	class rksla3_foxhound_lppv_base_pso_black: rksla3_foxhound_lppv_base
 	{
 		scope = 2;
 		scopeArsenal = 2;
@@ -22,10 +22,11 @@ class CfgVehicles
 		author = "Retextured by Corey";
 		displayName = "Foxhound LPPV PSO Black";
 		DLC = "";
-		hiddenSelectionsTextures[] = {"\pso_foxhound\paa\foxhound_exterior_pso_black.paa"};
+		hiddenSelections[] = {"main_texture","interior_texture","Plate_2","Plate_3","Plate_4"};
+		hiddenSelectionsTextures[] = {"pso_foxhound\paa\foxhound_exterior_pso_black_co.paa","pso_foxhound\paa\foxhound_interior_co.paa","pso_foxhound\paa\2_co.paa","pso_foxhound\paa\3_co.paa","pso_foxhound\paa\4_co.paa"};
 	};
 	
-	class rksla3_foxhound_lppv_tes_pso_olive: rksla3_foxhound_lppv_tes
+	class rksla3_foxhound_lppv_base_pso_olive: rksla3_foxhound_lppv_base
 	{
 		scope = 2;
 		scopeArsenal = 2;
@@ -33,10 +34,11 @@ class CfgVehicles
 		author = "Retextured by Corey";
 		displayName = "Foxhound LPPV PSO Olive Drab";
 		DLC = "";
-		hiddenSelectionsTextures[] = {"\pso_foxhound\paa\foxhound_exterior_pso_olive.paa"};
+		hiddenSelections[] = {"main_texture"};
+		hiddenSelectionsTextures[] = {"pso_foxhound\paa\foxhound_exterior_pso_olive_co.paa"};
 	};
 	
-	class rksla3_foxhound_lppv_tes_pso_tan: rksla3_foxhound_lppv_tes
+	class rksla3_foxhound_lppv_base_pso_tan: rksla3_foxhound_lppv_base
 	{
 		scope = 2;
 		scopeArsenal = 2;
@@ -44,10 +46,11 @@ class CfgVehicles
 		author = "Retextured by Corey";
 		displayName = "Foxhound LPPV PSO Tan";
 		DLC = "";
-		hiddenSelectionsTextures[] = {"\pso_foxhound\paa\foxhound_exterior_pso_tan.paa"};
+		hiddenSelections[] = {"main_texture"};
+		hiddenSelectionsTextures[] = {"pso_foxhound\paa\foxhound_exterior_pso_tan_co.paa"};
 	};
 	
-	class rksla3_foxhound_lppv_tes_pso_white: rksla3_foxhound_lppv_tes
+	class rksla3_foxhound_lppv_base_pso_white: rksla3_foxhound_lppv_base
 	{
 		scope = 2;
 		scopeArsenal = 2;
@@ -55,6 +58,7 @@ class CfgVehicles
 		author = "Retextured by Corey";
 		displayName = "Foxhound LPPV PSO White";
 		DLC = "";
-		hiddenSelectionsTextures[] = {"\pso_foxhound\paa\foxhound_exterior_pso_white.paa"};
+		hiddenSelections[] = {"main_texture","interior_texture","Plate_2","Plate_3","Plate_4"};
+		hiddenSelectionsTextures[] = {"pso_foxhound\paa\foxhound_exterior_pso_white_co.paa","pso_foxhound\paa\foxhound_interior_co.paa","pso_foxhound\paa\2_co.paa","pso_foxhound\paa\3_co.paa","pso_foxhound\paa\4_co.paa"};
 	};
 };
