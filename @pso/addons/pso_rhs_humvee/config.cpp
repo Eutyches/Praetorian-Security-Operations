de65e5f59a6728eb47dccc5e575d705d1d3733ef
big-corey <39989971+big-corey@users.noreply.github.com>
2019-11-10 07:36:02 -0600

    Humvee Gathering 0.2

--- @pso/addons/pso_rhs_humvee/config.cpp
+++ @pso/addons/pso_rhs_humvee/config.cpp
@@ -1,4 +1,6 @@
-/* Inherited Class Information // rhsusf_hmmwv
+/* 
+** Inherited Class Information // rhsusf_hmmwv **
+
 M998
 8 Seater, Open
 rhsusf_m998_d_2dr;
@@ -9,9 +11,14 @@ M1025
 4 Seater, Closed
 rhsusf_m1025_d;
 hiddenSelections[] = {"exterior","interior","A2","wheels","mainbody","hood gratting","camo1","camo2","unitdecals_1","unitdecals_2","ind_wait","ind_brake","ind_highbeam"};
-HiddenSelectionsTextures[] = {"rhsusf\addons\rhsusf_hmmwv\textures\m998_exterior_d_co.paa","rhsusf\addons\rhsusf_hmmwv\textures\m998_interior_d_co.paa","rhsusf\addons\rhsusf_hmmwv\textures\A2_parts_D_co.paa","rhsusf\addons\rhsusf_hmmwv\textures\wheel_wranglermt_d_co.paa","rhsusf\addons\rhsusf_hmmwv\textures\m998_mainbody_d_co.paa","rhsusf\addons\rhsusf_hmmwv\textures\gratting_d_co.paa","rhsusf\addons\rhsusf_hmmwv\textures\tile_exmetal_d_co.paa","rhsusf\addons\rhsusf_hmmwv\textures\m1025_d_co.paa","rhsusf\addons\rhsusf_hmmwv\unitdecals\101stab_502reg_2ndbn_a12_d_co.paa","rhsusf\addons\rhsusf_hmmwv\unitdecals\a12^_co.paa"};
+hiddenSelectionsTextures[] = {"rhsusf\addons\rhsusf_hmmwv\textures\m998_exterior_d_co.paa","rhsusf\addons\rhsusf_hmmwv\textures\m998_interior_d_co.paa","rhsusf\addons\rhsusf_hmmwv\textures\A2_parts_D_co.paa","rhsusf\addons\rhsusf_hmmwv\textures\wheel_wranglermt_d_co.paa","rhsusf\addons\rhsusf_hmmwv\textures\m998_mainbody_d_co.paa","rhsusf\addons\rhsusf_hmmwv\textures\gratting_d_co.paa","rhsusf\addons\rhsusf_hmmwv\textures\tile_exmetal_d_co.paa","rhsusf\addons\rhsusf_hmmwv\textures\m1025_d_co.paa","rhsusf\addons\rhsusf_hmmwv\unitdecals\101stab_502reg_2ndbn_a12_d_co.paa","rhsusf\addons\rhsusf_hmmwv\unitdecals\a12^_co.paa"};
 
-Retextures by Corey of Praetorian Security Operations.
+
+** GitHub/Praetorian-Security-Operations/Texture Templates/Humvee Retexturing Information **
+hiddenSelections[] = {"exterior","interior","A2","wheels","mainbody","hood gratting","camo1","camo2","unitdecals_1","unitdecals_2","ind_wait","ind_brake","ind_highbeam"};
+hiddenSelectionsTextures[] = {"pso_rhs_humvee\paa\a_exterior.paa","pso_rhs_humvee\paa\b_interior.paa","pso_rhs_humvee\paa\c_a2.paa","pso_rhs_humvee\paa\d_wheels.paa","pso_rhs_humvee\paa\e_mainbody.paa","pso_rhs_humvee\paa\f_gratting.paa","pso_rhs_humvee\paa\g_camo1_tile.paa","pso_rhs_humvee\paa\h_camo2_m1025.paa","pso_rhs_humvee\paa\i_unitdecals1.paa","pso_rhs_humvee\paa\j_unitdecals2.paa"};
+
+Most retextures by Corey of Praetorian Security Operations. Black exteriors by Dunbar Snackbar.
 */
 
 class CfgPatches
@@ -40,7 +47,7 @@ class CfgVehicles
 		displayName = "PSO Humvee Open 8 Seater Black";
 		DLC = "";
 		hiddenSelections[] = {"exterior","interior","A2","wheels","mainbody","hood gratting","camo1","camo2","unitdecals_1","unitdecals_2","ind_wait","ind_brake","ind_highbeam"};
-		hiddenSelectionsTextures[] = {"pso_rhs_humvee\paa\m998_exterior_d_co.paa","pso_rhs_humvee\paa\m998_interior_d_co.paa","pso_rhs_humvee\paa\A2_parts_D_co.paa","pso_rhs_humvee\paa\wheel_wranglermt_d_co.paa","pso_rhs_humvee\paa\m998_mainbody_d_co.paa","pso_rhs_humvee\paa\gratting_d_co.paa","pso_rhs_humvee\paa\tile_exmetal_d_co.paa","pso_rhs_humvee\paa\m1025_d_co.paa","pso_rhs_humvee\paa\101stab_502reg_2ndbn_a12_d_co.paa","pso_rhs_humvee\paa\a12^_co.paa"};
+		hiddenSelectionsTextures[] = {"pso_rhs_humvee\paa\a_exterior.paa","pso_rhs_humvee\paa\b_interior.paa","pso_rhs_humvee\paa\c_a2.paa","pso_rhs_humvee\paa\d_wheels.paa","pso_rhs_humvee\paa\e_mainbody.paa","pso_rhs_humvee\paa\f_gratting.paa","pso_rhs_humvee\paa\g_camo1_tile.paa","pso_rhs_humvee\paa\h_camo2_m1025.paa","pso_rhs_humvee\paa\i_unitdecals1.paa","pso_rhs_humvee\paa\j_unitdecals2.paa"};
 	};
 	
 	class rhsusf_m1025_pso_black: rhsusf_m1025_d;
@@ -52,6 +59,6 @@ class CfgVehicles
 		displayName = "PSO Humvee Closed 4 Seater Black";
 		DLC = "";
 		hiddenSelections[] = {"exterior","interior","A2","wheels","mainbody","hood gratting","camo1","camo2","unitdecals_1","unitdecals_2","ind_wait","ind_brake","ind_highbeam"};
-		hiddenSelectionsTextures[] = {"pso_rhs_humvee\paa\m998_exterior_d_co.paa","pso_rhs_humvee\paa\m998_interior_d_co.paa","pso_rhs_humvee\paa\A2_parts_D_co.paa","pso_rhs_humvee\paa\wheel_wranglermt_d_co.paa","pso_rhs_humvee\paa\m998_mainbody_d_co.paa","pso_rhs_humvee\paa\gratting_d_co.paa","pso_rhs_humvee\paa\tile_exmetal_d_co.paa","pso_rhs_humvee\paa\m1025_d_co.paa","pso_rhs_humvee\paa\101stab_502reg_2ndbn_a12_d_co.paa","pso_rhs_humvee\paa\a12^_co.paa"};
+		hiddenSelectionsTextures[] = {"pso_rhs_humvee\paa\a_exterior.paa","pso_rhs_humvee\paa\b_interior.paa","pso_rhs_humvee\paa\c_a2.paa","pso_rhs_humvee\paa\d_wheels.paa","pso_rhs_humvee\paa\e_mainbody.paa","pso_rhs_humvee\paa\f_gratting.paa","pso_rhs_humvee\paa\g_camo1_tile.paa","pso_rhs_humvee\paa\h_camo2_m1025.paa","pso_rhs_humvee\paa\i_unitdecals1.paa","pso_rhs_humvee\paa\j_unitdecals2.paa"};
 	};
 };
