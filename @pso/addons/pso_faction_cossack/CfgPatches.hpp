ad38d20e8957ead3d5958644a777df0509646258
big-corey <39989971+big-corey@users.noreply.github.com>
2019-11-27 19:59:14 -0600

    PSO Factions

---
+++ @pso/addons/pso_faction_cossack/CfgPatches.hpp
@@ -0,0 +1,69 @@
+class CfgPatches {
+    class O_RussiaInsurgents_Winter {
+        units[] = {
+            "O_RussiaInsurgents_Mi_8MT_VIV_Winter_01",
+            "O_RussiaInsurgents_Mi_8MT_Winter_01",
+            "O_RussiaInsurgents_Datsun_620_Pickup_PK_Woodland_Winter_01",
+            "O_RussiaInsurgents_DShKM_Winter_01",
+            "O_RussiaInsurgents_DSHkM_Minitripod_Winter_01",
+            "O_RussiaInsurgents_Datsun_620_Pickup_Woodland_Winter_01",
+            "O_RussiaInsurgents_SPG_9_Winter_01",
+            "O_RussiaInsurgents_Podnos_2B14_Winter_01",
+            "O_RussiaInsurgents_UAZ_Winter_01",
+            "O_RussiaInsurgents_Ural_Ammo_Winter_01",
+            "O_RussiaInsurgents_AA_IGLA_Pod_Winter_01",
+            "O_RussiaInsurgents_BTR_60PB_Winter_01",
+            "O_RussiaInsurgents_Rifleman_AK_74_Winter_01",
+            "O_RussiaInsurgents_Engineer_Winter_01",
+            "O_RussiaInsurgents_Rifleman_Winter_01",
+            "O_RussiaInsurgents_Ammo_Bearer_Winter_01",
+            "O_RussiaInsurgents_Grenadier_Winter_01",
+            "O_RussiaInsurgents_Officer_Winter_01",
+            "O_RussiaInsurgents_Medic_Winter_01",
+            "O_RussiaInsurgents_Commander_Winter_01",
+            "O_RussiaInsurgents_Automatic_Rifleman_Winter_01",
+            "O_RussiaInsurgents_Machinegunner_Winter_01",
+            "O_RussiaInsurgents_AT_Specialist_Winter_01",
+            "O_RussiaInsurgents_Rifleman_RPG_18_Winter_01",
+            "O_RussiaInsurgents_AA_Specialist_Winter_01",
+            "O_RussiaInsurgents_Sniper_Winter_01",
+            "O_RussiaInsurgents_Sapper_Winter_01",
+            "O_RussiaInsurgents_Saboteur_Winter_01",
+            "O_RussiaInsurgents_Ural_Repair_Winter_01",
+            "O_RussiaInsurgents_Ural_Refuel_Winter_01",
+            "O_RussiaInsurgents_Crewman_Winter_01",
+            "O_RussiaInsurgents_Pilot_Winter_01",
+            "O_RussiaInsurgents_Local_Winter_01",
+            "O_RussiaInsurgents_Woodman_Winter_01",
+            "O_RussiaInsurgents_Gamekeeper_Winter_01",
+            "O_RussiaInsurgents_Forester_Winter_01",
+            "O_RussiaInsurgents_Farmer_Winter_01",
+            "O_RussiaInsurgents_Villager_Winter_01",
+            "O_RussiaInsurgents_Ural_Open_Winter_01",
+            "O_RussiaInsurgents_Ural_Winter_01",
+            "O_RussiaInsurgents_Datsun_620_Pickup_PK_Winter_01",
+            "O_RussiaInsurgents_Rifleman_2_Winter_01",
+            "O_RussiaInsurgents_RHIB_Winter_01",
+            "O_RussiaInsurgents_Assault_Boat_Winter_01"
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {
+            "CUP_AirVehicles_Mi8",
+            "CUP_WheeledVehicles_Datsun",
+            "CUP_Weapons_StaticWeapons",
+            "CUP_WheeledVehicles_UAZ",
+            "CUP_WheeledVehicles_Ural",
+            "CUP_WheeledVehicles_BTR60",
+            "CUP_Creatures_Military_Chedaki",
+            "A3_Boat_F_Exp_Boat_Transport_02",
+            "ace_interaction",
+            "A3_Boat_F_Boat_Transport_01",
+            "A3_Boat_F_Exp_Boat_Transport_01",
+            "ace_refuel",
+            "ace_cargo"
+        };
+        author = "Pants";
+        authors[] = {"Pants"};
+    };
+};
